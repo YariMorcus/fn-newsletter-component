1ba3d2b7aa13e5fef6e6d2639cf02b0b8c31402b
YariMorcus <yarimorcus@zeelandnet.nl>
2023-12-13 14:20:29 +0100

    add: JSDoc comment

--- src/js/Views/subscribedView.js
+++ src/js/Views/subscribedView.js
@@ -1,6 +1,11 @@
 class subscribedView {
   #parentEl = document.querySelector('.subscribed');
 
+  /**
+   *
+   * @param {string} userEmail
+   * @returns {undefined}
+   */
   renderEmail(userEmail) {
     this.#parentEl.querySelector('#js-user-email').textContent =
       userEmail ?? '<user-email>';
